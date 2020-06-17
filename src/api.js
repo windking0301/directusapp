16cc3fef14af0df5615be4aad2512f245c86b103
windking0301 <33643671+windking0301@users.noreply.github.com>
2020-06-17 14:38:59 -0700

    update api

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import { SDK } from '@directus/sdk-js/dist/esm/index.js';
 
-const path = window.location.pathname;
+const path = 'https://worksdomain.nl/public' + window.location.pathname;
 const parts = path.split('/');
 const adminIndex = parts.indexOf('admin');
 const apiRootPath = parts.slice(0, adminIndex).join('/') + '/';
